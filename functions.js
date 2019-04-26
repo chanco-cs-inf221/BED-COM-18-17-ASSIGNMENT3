41468025b240f9acc3bbd0de1b815c11280db1c7
chifundo96 <48152358+chifundo96@users.noreply.github.com>
2019-04-26 13:01:37 +0200

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,60 @@
+function tsFunction(){
+    var h = "";
+   var e = [" distribut blood to the body",
+            "cells they act as a junction between", 
+            "alteries and veins so they ", 
+            "points of matreal exchange" ];
+    e.forEach(tsFunction);
+     document.getElementById("pra").innerHTML = h;
+     function tsFunction(value, index, array) {
+     h = h + value + "<br>"; 
+  }
+}
+function ysFunction(){
+    var h = "";
+   var e = ["this is the factory which manufactures",
+            "lots of proteins such as ezymes", 
+            "it controls what type of materials should", 
+            "circulate in the blood stream"];
+    e.forEach(ysFunction);
+     document.getElementById("pra").innerHTML = h;
+     function ysFunction(value, index, array) {
+     h = h + value + "<br>"; 
+  }
+}
+function psFunction(){
+    var t = "";
+   var g = ["these are tubes which carry blood from",
+            "heart rich in nutrients and oxygen except", 
+            "pulmonary artery which carries blood", 
+            "to the rungs to be oxyginated"];
+    g.forEach(psFunction);
+     document.getElementById("pra").innerHTML = t;
+     function psFunction(value, index, array) {
+     t = t + value + "<br>"; 
+  }
+}
+function vsFunction(){
+    var ax = "";
+   var ay = ["these are tubes which carry blood to the",
+            "heart poor in nutrients and oxygen except", 
+            "pulmonary vein which carries blood", 
+            "from the rungs full of oxygen to the heart"];
+    ay.forEach(vsFunction);
+     document.getElementById("pra").innerHTML = ax;
+     function vsFunction(value, index, array) {
+     ax = ax + value + "<br>"; 
+  }
+}
+function gsFunction(){
+    var ex = "";
+   var ary = ["this is where alot af wast materials",
+            "removed from the blood stream", 
+            "such as urine which contains lots of ", 
+            "useless and waste matrials"];
+    ary.forEach(gsFunction);
+     document.getElementById("pra").innerHTML = ex;
+     function gsFunction(value, index, array) {
+     ex = ex + value + "<br>"; 
+  }
+}
---
+++ javscrpt.js
@@ -0,0 +1,91 @@
+
+
+
+
+      
+       var p = "";
+       var s = ["The heart is rensposible for pumping", 
+                 "blood to provid force to push it ", 
+                  "and keep it moving around the body", 
+                 "click yellow box for more data about the heart ",];
+        s.forEach(myFunction);
+         document.getElementById("demo").innerHTML = p;
+         function myFunction(value, index, array) {
+         p = p + value + "<br>"; 
+      }
+
+        var u = "";
+       var r = ["the rungs cuptures oxygen from the atmosphere ", 
+            "into the body which react with haemoglobin to form", 
+            "oxyhaemoglobini inside the blood",
+            "click the left box to to see a lot "];
+        r.forEach(jfunction);
+         document.getElementById("id1a").innerHTML = u;
+         function jfunction(value, index, array) {
+         u = u + value + "<br>"; 
+      }
+      
+
+        var a = "";
+       var b = ["blood is the midiam that contains the mixture ", 
+               "of materials which are used by body cells", 
+                 "actually the blood is the source of raw materials",
+               "cells click this box if you want to major a lot"];
+        b.forEach(kfunction);
+         document.getElementById("id1b").innerHTML = a;
+         function kfunction(value, index, array) {
+         a = a + value + "<br>"; 
+      }
+      
+        var f = "";
+       var c = ["blood vessels are the pipe through which the", 
+                "blood moves, they are af three types grouped ", 
+                "volume, blood presure and the type of blood that they carry",
+                 "click this green box to appreciate its looks"];
+        c.forEach(Ffunction);
+         document.getElementById("id1c").innerHTML = f;
+         function Ffunction(value, index, array) {
+         f = f + value + "<br>"; 
+      }
+      
+      function isFunction(){
+        var j = "";
+       var d = ["right autrium pumps",
+                "blood to the ", 
+                " rungs to carry oxygen", 
+                 ];
+        d.forEach(isFunction);
+         document.getElementById("pra").innerHTML = j;
+         function isFunction(value, index, array) {
+         j = j + value + "<br>"; 
+      }
+    }
+      
+      function lsFunction(){
+        var l = "";
+       var n = ["left autrium pumps",
+                "blood to all", 
+                " pats of the body to distribute ", 
+                "neededmatreals" ];
+        n.forEach(lsFunction);
+         document.getElementById("pra").innerHTML = l;
+         function lsFunction(value, index, array) {
+         l = l + value + "<br>"; 
+      }
+    }
+      function xsFunction(){
+        var x = "";
+       var y = ["the rungs this is where ",
+                "oxygen is taken it links", 
+                " the atmosphere the great resevior", 
+                "all gases including oxygen" ];
+        y.forEach(xsFunction);
+         document.getElementById("pra").innerHTML = x;
+         function xsFunction(value, index, array) {
+         x = x + value + "<br>"; 
+      }
+    }
+
+ 
+   
+  
